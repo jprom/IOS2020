04dc193e8e03070e3e14bd10757b2296010f4210
jprom <33183303+jprom@users.noreply.github.com>
2020-03-28 09:27:45 -0600

    Quiz#3

---
+++ QuizCustomCell/QuizCuscomCell/QuizCuscomCell/CustomCell/tablaDelOchoTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  tablaDelOchoTableViewCell.swift
+//  QuizCuscomCell
+//
+//  Created by Pablo Rosales on 3/28/20.
+//  Copyright © 2020 Pablo Rosales. All rights reserved.
+//
+
+import UIKit
+
+class tablaDelOchoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellCustomCell: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+}
---
+++ QuizCustomCell/QuizCuscomCell/QuizCuscomCell/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  QuizCuscomCell
+//
+//  Created by Pablo Rosales on 3/28/20.
+//  Copyright © 2020 Pablo Rosales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
